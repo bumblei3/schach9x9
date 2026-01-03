2c9fe9e478a2593e0b5f29316360f86f07bb8476
bumblei3 <t.berberich@gmail.com>
2026-01-03 20:40:33 +0100

    fix: replace console.error with logger in storage.js and fix lint

--- tests/gameController.test.js
+++ tests/gameController.test.js
@@ -32,7 +32,7 @@ jest.unstable_mockModule('../js/sounds.js', () => ({
 
 jest.unstable_mockModule('../js/AnalysisController.js', () => ({
   AnalysisController: class {
-    constructor(gameController) {}
+    constructor(_gameController) { }
     enterAnalysisMode = jest.fn(() => true);
     exitAnalysisMode = jest.fn(() => true);
     requestPositionAnalysis = jest.fn();
@@ -268,7 +268,7 @@ describe('GameController', () => {
   describe('Clock Management', () => {
     it('should stop clock when not in PLAY phase', () => {
       game.phase = PHASES.SETUP_WHITE_KING;
-      gameController.timeManager.clockInterval = setInterval(() => {}, 100);
+      gameController.timeManager.clockInterval = setInterval(() => { }, 100);
       gameController.tickClock(); // Delegates to timeManager
       expect(gameController.timeManager.clockInterval).toBeNull();
     });
